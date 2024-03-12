106eacc104878e151cc1d097189baf0846bbd2b6
taishi <taishiarai@gmail.com>
2024-03-12 22:07:15 +0800

    Nav finished and peices of the hero image

--- index.php
+++ index.php
@@ -1,4 +1,56 @@
 <?php
 require('view/partials/head.php');
-
 require('view/partials/nav.php');
+?>
+
+<html>
+
+<body></body>
+<div>
+    <div class="bg-gradient-to-b from-indigo-950 to-indigo-900 py-5">
+        <div class="flex items-center pb-20">
+            <div class="flex-1 pl-60 ">
+                <h1 class="text-4xl font-bold text-white">Aircon Cleaning <br> Services Redefined</h1>
+                <p class="text-white mt-2">Cebu's premiere air conditioning <br> cleaning services</p>
+                <button class="bg-yellow-200 text-stone-900 duration-500 px-10 py-4 my-6 hover:bg-sky-300 rounded font-bold text-lg">Book Now</button>
+            </div>
+            <div class="flex-1 pr-52">
+                <img src="src/Coolhouse-hero.png" alt="coolhouse logo" style="max-width: 500px;">
+            </div>
+        </div>
+    </div>
+    <!-- This is the section for the features -->
+    <div class="flex gap-10 px-60 mt-[-10]">
+        <div class="flex-1 bg-sky-300 p-4 rounded-lg flex items-center flex-col">
+            <ion-icon name="snow-outline" class="text-6xl mb-2"></ion-icon>
+            <span class="text-center">Guaranteed
+                Cool <br> in every way</span>
+        </div>
+
+        <div class="flex-1 bg-sky-300 p-4 rounded-lg flex items-center flex-col">
+            <ion-icon name="snow-outline" class="text-6xl mb-2"></ion-icon>
+            <span class="text-center">We make sure
+                we <br> clean as we leave</span>
+        </div>
+
+        <div class="flex-1 bg-sky-300 p-4 rounded-lg flex items-center flex-col">
+            <ion-icon name="snow-outline" class="text-6xl mb-2"></ion-icon>
+            <span class="text-center">Fastest
+                Service</span>
+        </div>
+
+        <div class="flex-1 bg-sky-300 p-4 rounded-lg flex items-center flex-col">
+            <ion-icon name="snow-outline" class="text-6xl mb-2"></ion-icon>
+            <span class="text-center">Responsive
+                Customer <br> Service</span>
+        </div>
+    </div>
+
+    </body>
+
+
+</html>
+
+<?php
+// More PHP code if needed
+?>
--- view/partials/head.php
+++ view/partials/head.php
@@ -6,5 +6,7 @@
     <meta charset="UTF-8">
     <title>Demo</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <link rel="stylesheet" href="view/styles.css">
 </head>
